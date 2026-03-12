0248f54708db747e8a3929763272fd72b8d4c241
Mohammad Kevin <kvn4.200581@gmail.com>
2026-03-12 09:31:32 +0700

    fix upload and final

--- prisma/migrations/20260311030725_kosthunter/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE `kosfacility` DROP FOREIGN KEY `KosFacility_kos_id_fkey`;
-
--- AddForeignKey
-ALTER TABLE `KosFacility` ADD CONSTRAINT `KosFacility_kos_id_fkey` FOREIGN KEY (`kos_id`) REFERENCES `Kos`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
